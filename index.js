8c8a4ab216a7449134ef57657f14ab5c4f7a6d2c
Nithesh AN <nithuan141@gmail.com>
2017-08-08 15:29:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+    exports.toCamelCase = function (o) {
+        var newO, origKey, newKey, value
+        if (o instanceof Array) {
+            newO = []
+            for (origKey in o) {
+                value = o[origKey]
+                if (typeof value === "object") {
+                    value = this.toCamel(value)
+                }
+                newO.push(value)
+            }
+        } else {
+            newO = {}
+            for (origKey in o) {
+                if (o.hasOwnProperty(origKey)) {
+                    newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString()
+                    value = o[origKey]
+                    if (value !== null && (value.constructor === Object || value.constructor === Array)) {
+                        value = this.toCamel(value)
+                    }
+                    newO[newKey] = value
+                }
+            }
+        }
+        return newO;
+    }
